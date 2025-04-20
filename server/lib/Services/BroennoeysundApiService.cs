b5b3c17f81af3af66982f15f8318fa8ea52082c3
Bernt Andreas Eide <bernta1@msn.com>
2025-04-20 20:47:42 +0100

    Add rate limit on external API service

--- server/lib/Services/BroennoeysundApiService.cs
+++ server/lib/Services/BroennoeysundApiService.cs
@@ -9,6 +9,7 @@ public class BroennoeysundApiService
 {
     private readonly HttpClient _httpClient;
     private readonly string _baseUrl;
+    private static readonly SemaphoreSlim _semaphore = new(10, 10);
 
     public BroennoeysundApiService(HttpClient httpClient, IConfiguration config)
     {
@@ -18,11 +19,20 @@ public class BroennoeysundApiService
 
     public async Task<List<Company>> LookupCompanies(string name, int size = 100)
     {
+        if (string.IsNullOrEmpty(name))
+            return [];
+
+        try // simple "rate" limit, prevent excess search queries from clogging up the back-end.
+        {
+            await _semaphore.WaitAsync();
+        }
+        catch
+        {
+            return [];
+        }
+
         try
         {
-            if (string.IsNullOrEmpty(name))
-                return [];
-
             using var response = await _httpClient.GetAsync($"{_baseUrl}/api/enheter?navn={name}&size={size}");
             response.EnsureSuccessStatusCode();
 
@@ -37,6 +47,10 @@ public class BroennoeysundApiService
         catch
         {
             return [];
+        }
+        finally
+        {
+            _semaphore.Release();
         }
     }
 }
